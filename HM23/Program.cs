9c6da991637206597318a064a4548a616ce41a79
Oleg <elbrus14@mail.ru>
2022-09-08 17:24:41 +0300

    задача №23

---
+++ HM23/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+
+Console.Write($"{num} -> ");
+while (count < num)
+{
+Console.Write($"{Math.Pow((count), 3)}, ");
+count++;
+}
+
+if (count == num)
+{
+Console.Write($"{Math.Pow((count), 3)}");
+count++;
+}
+ 
